5c9bcf47e5262caf7cde08641f604eb1d3924892
Andres de Lago <delagoandres@gmail.com>
2019-02-17 13:33:39 -0600

    Added clang-format config and formatted all files

--- ExampleTournamentModule/Source/Dll.cpp
+++ ExampleTournamentModule/Source/Dll.cpp
@@ -3,19 +3,19 @@
 
 #include "ExampleTournamentModule.h"
 
-extern "C" __declspec(dllexport) void gameInit(BWAPI::Game* game) { BWAPI::BroodwarPtr = game; }
-
-BOOL APIENTRY DllMain(HANDLE, DWORD, LPVOID)
-{
-  return TRUE;
+extern "C" __declspec(dllexport) void gameInit(BWAPI::Game* game) {
+    BWAPI::BroodwarPtr = game;
 }
 
-extern "C" __declspec(dllexport) BWAPI::AIModule* newTournamentAI()
-{
-  return new ExampleTournamentAI();
+BOOL APIENTRY DllMain(HANDLE, DWORD, LPVOID) {
+    return TRUE;
 }
 
- extern "C" __declspec(dllexport) BWAPI::TournamentModule* newTournamentModule()
-{
-  return new ExampleTournamentModule();
+extern "C" __declspec(dllexport) BWAPI::AIModule* newTournamentAI() {
+    return new ExampleTournamentAI();
+}
+
+extern "C" __declspec(
+    dllexport) BWAPI::TournamentModule* newTournamentModule() {
+    return new ExampleTournamentModule();
 }
